4510412443d651b9421d24cd7a450ae0a8f22433
piasahmed1 <ahmedpias229@gmail.com>
2023-12-02 15:06:37 +0600

    added dashboard and all

--- src/main.jsx
+++ src/main.jsx
@@ -10,8 +10,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 // import AuthProvider from './Authintication/AuthProvider.jsx'
 const queryClient = new QueryClient()
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
