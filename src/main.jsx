d4be255e35897c938ad4b78a5a17ab45bc970355
piasahmed1 <ahmedpias229@gmail.com>
2023-12-02 15:06:14 +0600

    complete all section

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient()
 
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
